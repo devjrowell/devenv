91a513cba86264df4aa0dba92ba65b1cda870df9
Jon Rowell <jrowell@aptia.com>
2016-03-11 13:24:14 -0600

    supertab, vim-csharp, reogranized plugin installation

--- setup.sh
+++ setup.sh
@@ -2,20 +2,21 @@
 
 # oh-my-zsh
 cd ~/.devenv && git clone https://github.com/robbyrussell/oh-my-zsh.git
+cd ~/.devenv/oh-my-zsh && git submodule update --init --recursive
 
 # vim plugins
-cd ~/.devenv/vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git
-git clone https://github.com/Valloric/YouCompleteMe.git
-git clone https://github.com/kien/ctrlp.vim.git
-git clone https://github.com/scrooloose/nerdtree.git
-git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git
-git clone https://github.com/OmniSharp/omnisharp-vim.git
-git clone https://github.com/scrooloose/syntastic.git
-git clone https://github.com/vim-airline/vim-airline.git
-git clone https://github.com/vim-airline/vim-airline-themes.git
-git clone https://github.com/altercation/vim-colors-solarized.git
-git clone https://github.com/tpope/vim-dispatch.git
+#cd ~/.devenv/vim/bundle
+#git clone https://github.com/VundleVim/Vundle.vim.git
+#git clone https://github.com/Valloric/YouCompleteMe.git
+#git clone https://github.com/kien/ctrlp.vim.git
+#git clone https://github.com/scrooloose/nerdtree.git
+#git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git
+#git clone https://github.com/OmniSharp/omnisharp-vim.git
+#git clone https://github.com/scrooloose/syntastic.git
+#git clone https://github.com/vim-airline/vim-airline.git
+#git clone https://github.com/vim-airline/vim-airline-themes.git
+#git clone https://github.com/altercation/vim-colors-solarized.git
+#git clone https://github.com/tpope/vim-dispatch.git
 #exit
 
 # Omnisharp
@@ -30,6 +31,9 @@ git submodule update --init --recursive
 sudo npm install tern -g
 sudo npm install typescript -g
 ./install.py --clang-completer --omnisharp-completer --tern-completer
+
+# install all of the vim plugins
+vim -c PluginInstall
 
 # tmux plugins
 cd ~/.devenv/tmux/plugins
@@ -48,7 +52,4 @@ ln -s .devenv/tmux.conf .tmux.conf
 # vim
 ln -s .devenv/vim .vim
 ln -s .devenv/vimrc .vimrc
-#ln -s .devenv/vimrc.colors .vimrc.colors
-#ln -s .devenv/vimrc.omnisharp .vimrc.omnisharp
-#ln -s .devenv/vimrc.plugins .vimrc.plugins
 
